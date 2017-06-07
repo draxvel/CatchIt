9b2d7d3be3cb5cf52512e495aab5940276fba687
Nazar Tkachuk <ntv3331998@gmail.com>
2017-06-08 01:45:05 +0300

    release

---
+++ Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject Restart;
+
+    public static bool lose = false;
+
+    void Awake()
+    {
+        lose = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Bomb")
+        { 
+            lose = true;
+            Restart.SetActive(true);
+    }   }
+}
